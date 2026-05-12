12f6c6e4ecbdc7b61d0344c8a7293e1769814204
jire <jire@jires-MBP.lan>
2026-05-11 21:39:46 -0700

    Update website

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+SOURCE_DIR="$HOME/working/personal_page"
+PUBLISH_DIR="$HOME/working/jireren.github.io"
+DOMAIN="jingyao.ren"
+
+cd "$SOURCE_DIR"
+
+bundle exec jekyll build
+
+# Add GitHub Pages static-hosting files directly into generated output
+echo "$DOMAIN" > "$SOURCE_DIR/_site/CNAME"
+touch "$SOURCE_DIR/_site/.nojekyll"
+
+rsync -av --delete \
+  --exclude='.git/' \
+  "$SOURCE_DIR/_site/" "$PUBLISH_DIR/"
+
+cd "$PUBLISH_DIR"
+
+git add .
+git commit -m "Update website" || echo "No changes to commit"
+git push origin main
